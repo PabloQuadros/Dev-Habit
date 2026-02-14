1d5e2bb3d45eb24a88b51d63e0a3f9491837c819
PabloQuadros <pablo.aquadros@gmail.com>
2026-02-13 23:24:23 -0300

    feat: Add Habit entity

--- DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Database;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public DbSet<Habit> Habits { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
