d9f5013fdeb3faf23e0ad35d841e16e31a56551e
PabloQuadros <pablo.aquadros@gmail.com>
2026-02-22 14:36:35 -0300

    feat: Add dynamic sorting

--- DevHabit/DevHabit.Api/Controllers/HabitController.cs
+++ DevHabit/DevHabit.Api/Controllers/HabitController.cs
@@ -1,6 +1,7 @@
 ﻿using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
+using DevHabit.Api.Services.Sorting;
 using FluentValidation;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +14,21 @@ namespace DevHabit.Api.Controllers;
 public sealed class HabitController(ApplicationDbContext dbContext) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<HabitsCollectionDto>> GetHabits([FromQuery]HabitsQueryParameters query)
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabits(
+        [FromQuery]HabitsQueryParameters query,
+        SortMappingProvider sortMappingProvider)
     {
+        if (!sortMappingProvider.ValidateMappings<HabitDto, Habit>(query.Sort))
+        {
+            return Problem (
+                statusCode: StatusCodes.Status400BadRequest,
+                detail: $"The provided sort parameter isn't valid: '{query.Sort}'");
+        }
+
         query.Search ??= query.Search?.Trim().ToLower();
         
+        SortMapping[] sortMappings = sortMappingProvider.GetMappings<HabitDto,Habit>();
+
         List<HabitDto> habits = await dbContext.Habits
             .Where(h => 
                 query.Search == null || 
@@ -25,6 +37,7 @@ public sealed class HabitController(ApplicationDbContext dbContext) : Controller
                 h.Description.ToLower().Contains(query.Search))
             .Where(h => query.Type == null || h.Type == query.Type)
             .Where(h => query.Status == null || h.Status == query.Status)
+            .ApplySort(query.Sort, sortMappings)
             .Select(HabitQueries.ProjectToDto())
             .ToListAsync();
 
