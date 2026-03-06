b16416f3e165573dbe29d9d7f152dd3fe59a8095
PabloQuadros <pablo.aquadros@gmail.com>
2026-03-06 20:42:45 -0300

    feat: Add datashaping

--- DevHabit/DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
+++ DevHabit/DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
@@ -10,6 +10,7 @@ public sealed record HabitsQueryParameters
     public HabitType? Type { get; set; }
     public HabitStatus? Status { get; set; }
     public string? Sort { get; init; }
+    public string? Fields { get; init; }
     public int Page { get; set; } =1;
     public int PageSize { get; set; } = 10;
 }
