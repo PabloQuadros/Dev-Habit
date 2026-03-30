84d4ca311e3043dc25d0da749cbd3ade83e492ca
PabloQuadros <pablo.aquadros@gmail.com>
2026-03-30 06:31:42 -0300

    feat: Register User

---
+++ DevHabit/DevHabit.Api/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Auth;
+using DevHabit.Api.DTOs.Users;
+using DevHabit.Api.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+[AllowAnonymous]
+public sealed class AuthController(
+    UserManager<IdentityUser> userManager,
+    ApplicationIdentityDbContext identityDbContext,
+    ApplicationDbContext applicationDbContext) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+    {
+        //Use the same transaction in our dbContexts
+        using IDbContextTransaction transection = await identityDbContext.Database.BeginTransactionAsync();
+        applicationDbContext.Database.SetDbConnection(identityDbContext.Database.GetDbConnection());
+        await applicationDbContext.Database.UseTransactionAsync(transection.GetDbTransaction());
+        
+        //Create Identity User
+        var identityUser = new IdentityUser
+        {
+            Email = registerUserDto.Email,
+            UserName = registerUserDto.Name
+        };
+
+        IdentityResult identityResult = await userManager.CreateAsync(identityUser, registerUserDto.Password);
+
+        if (!identityResult.Succeeded)
+        {
+            var extensions = new Dictionary<string, object?>
+            {
+                {
+                    "errors",
+                    identityResult.Errors.ToDictionary(e => e.Code, e => e.Description)
+                }
+            };
+
+            return Problem(
+                detail: "Unable to register user, please try again",
+                statusCode: StatusCodes.Status400BadRequest,
+                extensions: extensions);
+        }
+        
+        //Create application user
+        User user = registerUserDto.ToEntity();
+        user.IdentityId = identityUser.Id;
+        
+        applicationDbContext.Users.Add(user);
+        
+        await applicationDbContext.SaveChangesAsync();
+        
+        await transection.CommitAsync();
+
+        return Ok(user.Id);
+    }
+}
